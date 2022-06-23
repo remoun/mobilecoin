29967d4d00453221cbace9973099c0a921029901
Remoun Metyas <remoun@mobilecoin.com>
2022-06-23 00:29:01 -0700

    fixup! DRY up build scripts that call mc_util_build_grpc

--- api/build.rs
+++ api/build.rs
@@ -6,8 +6,9 @@ fn main() {
     let env = Environment::default();
 
     let proto_dir = env.dir().join("proto");
+    let proto_str = proto_dir.to_str().unwrap();
     // Other crates depend on this output for their deps.
-    cargo_emit::pair!("PROTOS_PATH", "{:?}", proto_dir);
+    cargo_emit::pair!("PROTOS_PATH", "{}", proto_str);
 
     mc_util_build_grpc::compile_protos_and_generate_mod_rs(
         &[proto_dir],
--- consensus/api/build.rs
+++ consensus/api/build.rs
@@ -1,25 +1,25 @@
 // Copyright (c) 2018-2022 The MobileCoin Foundation
 
 use mc_util_build_script::Environment;
-use std::path::Path;
 
 fn main() {
     let env = Environment::default();
 
     let proto_dir = env.dir().join("proto");
+    let proto_str = proto_dir.to_str().unwrap();
     // Other crates depend on this output for their deps.
-    cargo_emit::pair!("PROTOS_PATH", "{:?}", proto_dir);
-
-    let attest_proto_path = env
-        .depvar("MC_ATTEST_API_PROTOS_PATH")
-        .expect("Could not read attest api's protos path");
-    let mut all_proto_dirs = attest_proto_path.split(':').collect::<Vec<&Path>>();
-    all_proto_dirs.push(&proto_dir);
+    cargo_emit::pair!("PROTOS_PATH", "{}", proto_str);
+    let mut all_proto_dirs = vec![proto_str];
 
     let api_proto_path = env
         .depvar("MC_API_PROTOS_PATH")
-        .expect("Could not read api's protos path");
+        .expect("Could not read mc_api's protos path");
     all_proto_dirs.extend(api_proto_path.split(':'));
+
+    let attest_proto_path = env
+        .depvar("MC_ATTEST_API_PROTOS_PATH")
+        .expect("Could not read mc_attest_api's protos path");
+    all_proto_dirs.extend(attest_proto_path.split(':'));
 
     mc_util_build_grpc::compile_protos_and_generate_mod_rs(
         all_proto_dirs.as_slice(),
